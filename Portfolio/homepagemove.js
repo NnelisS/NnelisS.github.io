d5980a02120c9b92d33fcea52408206bc1a18478
NnelisS <78425965+NnelisS@users.noreply.github.com>
2023-04-14 16:15:37 +0200

    Index files

---
+++ Portfolio/homepagemove.js
@@ -0,0 +1,16 @@
+// Load the YouTube API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/embed/9NrTCwsaEm0?";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create a new player object
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    playerVars: {
+      controls: 0 // Disable the player's controls
+    }
+  });
+}
